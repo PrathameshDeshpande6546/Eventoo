386b69e0b3ae343a754024004a1390df4b292b45
Prathamesh tatyaso Koppe <130206582+Prathameshkoppe@users.noreply.github.com>
2024-04-21 19:29:07 +0530

    initial uploads

---
+++ src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './component/header/header.component';
+
+
+const routes: Routes = [
+{ path: 'header', component:HeaderComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
+export { RouterModule, routes };
+
